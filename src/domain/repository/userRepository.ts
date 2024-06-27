d8babad12e5b2630eed2c04042ff2e50002feb54
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-06-26 22:25:12 -0300

    [346] - Criando a base da criação de adm

--- src/domain/repository/userRepository.ts
+++ src/domain/repository/userRepository.ts
@@ -1,7 +1,7 @@
 import { User } from "../entity/user";
 
 export interface UserRepository {
-    findByEmail(email:string): Promise<User | Error>;
-    create(user:User): Promise<User | Error>;
+    findByEmail(email:string):Promise<any>;
+    create(user:User,isAdm:boolean): Promise<User | Error>;
     delete(email:string): Promise<User | Error>;
 }
