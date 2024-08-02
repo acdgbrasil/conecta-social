18edf5a485f64fb7a5798ea0aa4b6ed7c0f0653f
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-08-02 12:28:47 -0300

    [357] - Reset password com um id protegido.

--- src/presenter/routers/authRouter.ts
+++ src/presenter/routers/authRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { AuthController } from "../../useCase/controllers/authController";
 import { CustomError } from "../../infra/error/error";
+import { UserController } from "../../useCase/controllers/userController";
+import { User } from "../../domain/entity/user";
 
 const authRouter = Router();
 const authController = new AuthController();
+const userController = new UserController();
 
 authRouter.post('/auth/login', async (req, res) => {
     try{
@@ -40,10 +43,13 @@ authRouter.post('/auth/forgot/password', async (req, res) => {
 
 authRouter.post('/auth/reset/password', async (req, res) => {
     try{
-        const {email,code,newPassword} = req.body;
+        const {id,email,code,newPassword} = req.body;
         if(!email) throw new CustomError('Bad Request',400,'Bad Request','Email is required');
         if(!code) throw new CustomError('Bad Request',400,'Bad Request','Code is required');
         if(!newPassword) throw new CustomError('Bad Request',400,'Bad Request','New password is required');
+        if(!id) throw new CustomError('Bad Request',400,'Bad Request','id is required');
+        const user = await userController.findByEmail(email) as User
+        if(user.id != id) throw new CustomError('Bad Request',400,'Bad Request','the Email are request is diferent to email has sended');
         const response = await authController.resetPassword(email,code,newPassword);
         return res.status(200).json(response);
     }catch(e){
