819b42bac530d004f867be2833c825514097f6af
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-08-05 14:31:51 -0300

    [362] - Correções de ingles.

--- src/presenter/routers/admRouter.ts
+++ src/presenter/routers/admRouter.ts
@@ -12,7 +12,7 @@ admRouter.get('/adm/list/all/:admEmail',async (req,res)=>{
         const superAdmEmail = process.env.SUPER_ADM_EMAIL
         const isSuperAd = superAdmEmail === admEmail
         if(!isSuperAd){
-            throw new CustomError('Bad Request',401,'Bad Request','Your access is denied because, because your not allowed');
+            throw new CustomError('Bad Request',401,'Bad Request','Your access is denied, because you are not allowed');
         }
         
         const users = await admController.listAllUsers()
--- src/presenter/routers/authRouter.ts
+++ src/presenter/routers/authRouter.ts
@@ -49,7 +49,7 @@ authRouter.post('/auth/reset/password', async (req, res) => {
         if(!newPassword) throw new CustomError('Bad Request',400,'Bad Request','New password is required');
         if(!id) throw new CustomError('Bad Request',400,'Bad Request','id is required');
         const user = await userController.findByEmail(email) as User
-        if(user.id != id) throw new CustomError('Bad Request',400,'Bad Request','the Email are request is diferent to email has sended');
+        if(user.id != id) throw new CustomError('Bad Request',400,'Bad Request','the Email requested is diferent to email has sended');
         const response = await authController.resetPassword(email,code,newPassword);
         return res.status(200).json(response);
     }catch(e){
