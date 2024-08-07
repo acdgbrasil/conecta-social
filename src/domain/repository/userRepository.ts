af75fd82d4c0cb02e980caee2f5e211d4a1ede91
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-08-07 16:31:42 -0300

    [08] - Adicionando o referencePersonRepository.

--- src/domain/repository/userRepository.ts
+++ src/domain/repository/userRepository.ts
@@ -1,7 +1,9 @@
+import { ReferencePerson } from "../entity/referencePerson";
 import { User } from "../entity/user";
 
 export interface UserRepository {
     findByEmail(email:string):Promise<any>;
     create(user:User,isAdm:boolean): Promise<User | Error>;
     delete(email:string): Promise<User | Error>;
+    createReferencePerson(referencePerson:ReferencePerson): Promise<ReferencePerson | Error>;
 }
