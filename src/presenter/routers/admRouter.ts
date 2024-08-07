9fcc4ebd9d28402e777501c02b6cd126129206d9
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-08-07 15:05:31 -0300

    [362] - Corrigindo mensagem de erro errada.

--- src/presenter/routers/admRouter.ts
+++ src/presenter/routers/admRouter.ts
@@ -12,7 +12,7 @@ admRouter.get('/adm/list/all/:admEmail',async (req,res)=>{
         const superAdmEmail = process.env.SUPER_ADM_EMAIL
         const isSuperAd = superAdmEmail === admEmail
         if(!isSuperAd){
-            throw new CustomError('Bad Request',401,'Bad Request','Your access is denied, because you are not allowed');
+            throw new CustomError('Unauthorized',401,'Unauthorized','Your access is denied, because you are not allowed');
         }
         
         const users = await admController.listAllUsers()
@@ -33,7 +33,7 @@ admRouter.patch('/adm/deactivate/user',async(req,res)=>{
         const superAdmEmail = process.env.SUPER_ADM_EMAIL
         const isSuperAd = superAdmEmail === admEmail
         if(!isSuperAd){
-            throw new CustomError('Bad Request',401,'Bad Request','Your access is denied, because you are not allowed');
+            throw new CustomError('Unauthorized',401,'Unauthorized','Your access is denied, because you are not allowed');
         }
         
         const hasSuccesfull = await admController.deactivateUser(email)
