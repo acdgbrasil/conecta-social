7fc45212928d6ea361d13cf256f7ac03c2925759
Gabriel Aderaldo <gabriel_aderaldo@edu.unifor.br>
2025-01-29 16:25:10 -0300

    [107] - Criando as regras de adicionar pregnants só quando é do genero feminino.

--- src/infra/database/mongodb/mongoDtos/familyCompositionDto.ts
+++ src/infra/database/mongodb/mongoDtos/familyCompositionDto.ts
@@ -1,5 +1,5 @@
 import { get } from "http";
-import { Documents, EducationConditionPerson, FamilyCompositionPerson, WorkConditionPerson } from "../../../../domain/entity/familyComposition";
+import { Documents, EducationConditionPerson, FamilyCompositionPerson, Pregnant, WorkConditionPerson } from "../../../../domain/entity/familyComposition";
 import { Observations } from "../../../../domain/entity/observations";
 import { CustomError } from "../../../error/error";
 import { familyCompositionModel } from "../models/familyCompositionModel";
@@ -115,7 +115,11 @@ export const createPregnant = async (pregnant:any,familyCompositionID:string,id:
         if(!familyComposition) throw new CustomError('FAMILY_COMPOSITION_NOT_FOUND',404,'FAMILY_COMPOSITION_NOT_FOUND','Family Composition not found')
         const familyCompositionPerson = familyComposition.familyCompositionPerson.find((person:any) => person._id == id)
         if(!familyCompositionPerson) throw new CustomError('FAMILY_COMPOSITION_PERSON_NOT_FOUND',404,'FAMILY_COMPOSITION_PERSON_NOT_FOUND','Family Composition Person not found')
-        familyCompositionPerson.pregnant = pregnant
+        if(familyCompositionPerson.biologicalGender == "Masculino") {
+            familyCompositionPerson.pregnant = undefined
+        }else{
+            familyCompositionPerson.pregnant = pregnant
+        }
         familyComposition.isInUse = true
         familyComposition.save()
         return familyComposition
