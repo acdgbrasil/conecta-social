9e34c4b0ac260cfb1aa92eb52246bf56546d8529
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2025-06-30 20:47:51 -0300

    [reliase/1.1.0] - Criando sempre que tiver uma migration, criar um userAdm novo.

--- src/infra/database/postgress/migrations/postMigrations.ts
+++ src/infra/database/postgress/migrations/postMigrations.ts
@@ -1,6 +1,10 @@
 import { PoolClient } from "pg";
 import { CREATE_TABLE_USER } from "../schema/userSchema.ts";
+import { createADM } from "../postgressDTO.ts";
+import { User } from "../../../../domain/entity/user.ts";
 
 export const migration_25_05_2025 = async (pgClient:PoolClient) =>{
     const result = await pgClient.query(CREATE_TABLE_USER);
+    const userAdm = new User(1,process.env.SUPER_ADM_NAME || 'Gabriel Aderaldo', process.env.SUPER_ADM_EMAIL || 'defaut',process.env.SUPER_ADM_PASSWORD || 'default', null, 'admin', new Date(), new Date(), true);
+    await createADM(userAdm);
 }
