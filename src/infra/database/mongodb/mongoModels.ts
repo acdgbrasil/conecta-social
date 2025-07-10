709b0c814f5050abf35aff299d51e345a679a32d
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2025-07-10 11:40:54 -0300

    [reliase] - Vendo o que tá dando na dev

--- src/infra/database/mongodb/mongoModels.ts
+++ src/infra/database/mongodb/mongoModels.ts
@@ -7,15 +7,18 @@ const codeSchema = new mongoose.Schema({
     code: {
         type: String,
         required: true
-      },
-      createdAt: { type: Date, expires: FIVE_MINUTES, default: Date.now,index:true },
-      expireAt: {
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+    expireAt: {
         type: Date,
         expires: FIVE_MINUTES,
         default: Date.now
     }
 });
 
+codeSchema.index({ createdAt: 1 }, { expireAfterSeconds: FIVE_MINUTES });
 
-codeSchema.index({createdAt: 1},{expireAfterSeconds: FIVE_MINUTES});
 export const CodeModel = mongoose.model('Code', codeSchema);
